924c73d1539a7570bd13fae50bdc24f5e3c37a79
Tahar Moustalik <tahar-moustalik>
2020-03-16 15:54:12 +0100

    Add Product List

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -4,10 +4,55 @@
 namespace App\Controller;
 
 
+use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class ProductController extends AbstractController
 {
+
+
+    /**
+     * @Route("/product",name="product_list")
+     */
+    public function index() {
+        $products = [
+            new Product(1, "Laptop", 9000, 2, "Laptop", "placeholder image",
+                new \DateTime('2017-10-17', new \DateTimeZone('Europe/Paris'))),
+            new Product(2, "Machine a laver", 5000, 22, "Samsung", "placeholder image",
+                new \DateTime('2017-10-17', new \DateTimeZone('Europe/Paris'))),
+            new Product(3, "Iphones", 9000, 50, "Apple SmartPhones", "placeholder image",
+                new \DateTime('2017-10-17', new \DateTimeZone('Europe/Paris')))
+        ];
+        return $this->render('product/index.html.twig',[
+            'products' => $products
+        ]);
+    }
+
+
+    /**
+     * @return Response
+     * @Route("/product/add",name="product_add")
+     */
+    public function add() {
+        return $this->render('product/add.html.twig');
+    }
+
+
+    /**
+     * @Route("/product/edit",name="product_edit")
+     */
+    public function edit() {
+        return $this->render('product/edit.html.twig');
+    }
+
+    /**
+     * @Route("/product/show",name="product_show")
+     */
+    public function show() {
+        return $this->render('product/show.html.twig');
+    }
 
 
 
---
+++ src/Entity/Product.php
@@ -0,0 +1,153 @@
+<?php
+
+
+namespace App\Entity;
+
+
+class Product
+{
+
+    private $id;
+    private $name;
+    private $price;
+    private $quantity;
+    private $description;
+    private $imageUrl;
+    private $createdAt;
+
+    /**
+     * Product constructor.
+     * @param $id
+     * @param $name
+     * @param $price
+     * @param $quantity
+     * @param $description
+     * @param $imageUrl
+     * @param $createdAt
+     */
+    public function __construct($id, $name, $price, $quantity, $description, $imageUrl, $createdAt)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->price = $price;
+        $this->quantity = $quantity;
+        $this->description = $description;
+        $this->imageUrl = $imageUrl;
+        $this->createdAt = $createdAt;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * @param mixed $price
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getQuantity()
+    {
+        return $this->quantity;
+    }
+
+    /**
+     * @param mixed $quantity
+     */
+    public function setQuantity($quantity)
+    {
+        $this->quantity = $quantity;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param mixed $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getImageUrl()
+    {
+        return $this->imageUrl;
+    }
+
+    /**
+     * @param mixed $imageUrl
+     */
+    public function setImageUrl($imageUrl)
+    {
+        $this->imageUrl = $imageUrl;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param mixed $createdAt
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+    }
+
+
+
+}
